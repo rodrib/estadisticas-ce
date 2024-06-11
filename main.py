2242298cdce3cfd5dc1ab7220350a3f20701af3d
rodrib <rodribogado50@gmail.com>
2024-06-11 20:56:34 -0300

    update tabs in app

--- main.py
+++ main.py
@@ -231,6 +231,24 @@ st.header("Tabs")
 
 
 
+# import streamlit as st
+# import streamlit_shadcn_ui as ui
+
+# value = ui.tabs(options=['PyGWalker', 'Graphic Walker'], default_value='PyGWalker', key="kanaries")
+
+# with ui.card(key="image"):
+#     if value == "PyGWalker":
+#         ui.element("img", src="https://pub-8e7aa5bf51e049199c78b4bc744533f8.r2.dev/pygwalker-banner.png", className="w-full")
+#         ui.element("link_button", text=value + " Github", url="https://github.com/Kanaries/pygwalker", className="mt-2", key="btn2")
+#     elif value == "Graphic Walker":
+#         ui.element("img", src="https://pub-8e7aa5bf51e049199c78b4bc744533f8.r2.dev/graphic-walker-banner.png", className="w-full")
+#         ui.element("link_button", text=value + " Github", url="https://github.com/Kanaries/graphic-walker", className="mt-2", key="btn2")
+
+# # Mostrar el contenido de las pestañas
+# st.write(ui.tabs)
+
+
+########
 import streamlit as st
 import streamlit_shadcn_ui as ui
 
@@ -238,7 +256,7 @@ value = ui.tabs(options=['PyGWalker', 'Graphic Walker'], default_value='PyGWalke
 
 with ui.card(key="image"):
     if value == "PyGWalker":
-        ui.element("img", src="miseq-imagen.png", className="w-full")
+        st.image("miseq-imagen.png", caption="PyGWalker", use_column_width=True)
         ui.element("link_button", text=value + " Github", url="https://github.com/Kanaries/pygwalker", className="mt-2", key="btn2")
     elif value == "Graphic Walker":
         ui.element("img", src="https://pub-8e7aa5bf51e049199c78b4bc744533f8.r2.dev/graphic-walker-banner.png", className="w-full")
