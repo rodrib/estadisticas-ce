c7262dd5cf468850ba67a49882c2a411bdf7512d
rodrib <rodribogado50@gmail.com>
2024-07-06 00:11:45 -0300

    test columns

--- pages/03_test_columns.py
+++ pages/03_test_columns.py
@@ -1,43 +1,15 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 
-# Especifica la ruta del archivo CSV
-file_path = 'comite-etl-all.csv'
-
-# Leer el archivo CSV y convertirlo en un DataFrame
-df = pd.read_csv(file_path)
-
-# Eliminar las filas con valores nulos
-df = df.dropna()
-
-# Inicializar el estado de sesión si no está ya inicializado
-if "df_selection" not in st.session_state:
-    st.session_state.df_selection = {"selected_rows": [], "selected_columns": []}
-
-# Muestra el DataFrame con la selección habilitada
-selected_points = st.dataframe(
-    df,
-    key="df",
-    height=300
+df = pd.DataFrame(
+    [
+        {"command": "st.selectbox", "rating": 4, "is_widget": True},
+        {"command": "st.balloons", "rating": 5, "is_widget": False},
+        {"command": "st.time_input", "rating": 3, "is_widget": True},
+    ]
 )
 
-# Obtener las filas y columnas seleccionadas
-selected_rows = st.session_state.df_selection["selected_rows"]
-selected_columns = st.session_state.df_selection["selected_columns"]
+edited_df = st.data_editor(df) # 👈 An editable dataframe
 
-# Filtra el DataFrame original para obtener los datos seleccionados
-if selected_rows and selected_columns:
-    selected_data = df.iloc[selected_rows, selected_columns]
-    
-    # Mostrar los datos seleccionados
-    st.write("Datos seleccionados:")
-    st.dataframe(selected_data)
-    
-    # Guardar los datos seleccionados en un archivo CSV
-    selected_data.to_csv('selected_data.csv', index=False)
-    st.write("Datos seleccionados guardados en 'selected_data.csv'.")
-
-# Mostrar el estado de selección en formato JSON
-st.json(st.session_state.df_selection)
-
-
+favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
+st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
